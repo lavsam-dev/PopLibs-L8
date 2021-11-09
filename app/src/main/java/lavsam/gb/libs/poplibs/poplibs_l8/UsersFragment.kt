4124f25f60363a2339a3a62d5eb7ae0790f8426f
lavsam-dev <lavsam@gmail.com>
2021-11-09 19:11:40 +0400

    Users*

---
+++ app/src/main/java/lavsam/gb/libs/poplibs/poplibs_l8/UsersFragment.kt
@@ -0,0 +1,67 @@
+package lavsam.gb.libs.poplibs.poplibs_l8
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
+import lavsam.gb.libs.poplibs.poplibs_l8.databinding.FragmentUsersBinding
+import moxy.MvpAppCompatFragment
+import moxy.ktx.moxyPresenter
+import javax.inject.Inject
+
+class UsersFragment : MvpAppCompatFragment(), UsersView, BackButtonListener {
+    companion object {
+        fun newInstance() = UsersFragment().apply {
+            App.instance.appComponent.inject(this)
+        }
+    }
+
+    @Inject
+    lateinit var somePresenter: UsersPresenter
+
+    val presenter: UsersPresenter by moxyPresenter {
+        somePresenter
+    }
+
+    var adapter: UsersRVAdapter? = null
+    private var vb: FragmentUsersBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ) =
+        FragmentUsersBinding.inflate(inflater, container, false).also {
+            vb = it
+        }.root
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        vb = null
+    }
+
+    override fun init() {
+        vb?.run {
+            this.rvUsers.layoutManager = LinearLayoutManager(context)
+            adapter = UsersRVAdapter(presenter.usersListPresenter)
+            this.rvUsers.adapter = adapter
+        }
+
+        vb?.image?.run {
+            Glide.with(this)
+                .load("https://news102.ru/wp-content/uploads/2020/07/july.png")
+                .placeholder(R.drawable.ic_launcher_foreground_2)
+                .into(this)
+        }
+
+
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun updateList() {
+        adapter?.notifyDataSetChanged()
+    }
+
+    override fun backPressed() = presenter.backPressed()
+}
---
+++ app/src/main/java/lavsam/gb/libs/poplibs/poplibs_l8/UsersPresenter.kt
@@ -0,0 +1,72 @@
+package lavsam.gb.libs.poplibs.poplibs_l8
+
+import android.util.Log
+import com.github.terrakok.cicerone.Router
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import moxy.MvpPresenter
+import javax.inject.Inject
+
+class UsersPresenter @Inject constructor(
+    private val usersRepo: GithubUsersRepo,
+    private val router: Router,
+    private val screens: IScreens
+) : MvpPresenter<UsersView>() {
+
+    init {
+        App.instance.appComponent.inject(this)
+    }
+
+
+    class UsersListPresenter : IUserListPresenter {
+
+        val users = (1..20).map { GithubUser("login $it", 0) }.toMutableList()
+
+        override fun getUserByPosition(position: Int): GithubUser {
+            return users[position]
+        }
+
+        override var itemClickListener: ((UserItemView, GithubUser) -> Unit)? = null
+        override fun getCount() = users.size
+        override fun bindView(view: UserItemView) {
+            val user = users[view.pos]
+            view.setLogin(user.login)
+        }
+    }
+
+    val usersListPresenter = UsersListPresenter()
+
+    private val disposable = usersRepo.subscribeOnGithubUsersData()
+        .observeOn(AndroidSchedulers.mainThread())
+        .subscribe({
+            usersListPresenter.users.clear()
+            usersListPresenter.users.addAll(it)
+            viewState.updateList()
+        }, {
+            Log.e("GB_LIBS", it.message, it)
+        })
+
+    override fun onFirstViewAttach() {
+        super.onFirstViewAttach()
+        viewState.init()
+        usersRepo.loadUserData()
+        usersListPresenter.itemClickListener = { _, user ->
+            router.navigateTo(
+                screens.oneUser(
+                    userAvatarUrl = user.avatarUrl,
+                    userName = user.login
+                )
+            )
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable.dispose()
+    }
+
+
+    fun backPressed(): Boolean {
+        router.exit()
+        return true
+    }
+}
